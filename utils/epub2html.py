bd7eb34d3eedb4ced5b7d9e601377296067f324c
papa <visidoor@yandex.ru>
2022-12-15 21:29:57 +0500

    add nav block

--- utils/epub2html.py
+++ utils/epub2html.py
@@ -17,6 +17,15 @@ def convert(path):
             else: print('Enter "y" for "yes" or "n" for "no"')
     html_dir.mkdir(exist_ok=True)
     book = epub.read_epub(path)
+    arr = []
+    iArr = 0
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            arr.append(item.get_name())
+            iArr = iArr + 1
+    leng = len(arr)
+    jArr = 0
+    a3 = ''
     for item in book.get_items():
         doc = html_dir / item.get_name()
         os.makedirs(os.path.dirname(doc), exist_ok=True)
@@ -28,6 +37,15 @@ def convert(path):
                     content = content.decode()
                     content = re.sub(r'<a (.+?)\/>', r'<a \1></a>', content)
                     content = re.sub(r'<head/>', r'<head><title>%s</title><script type="text/javascript" src="/js-and-css/js/inject.js"></script><script type="text/javascript" src="file:///media/storage418Gb/Users/parsh/Documents/Books/js-and-css/js/inject.js"></script><script type="text/javascript" src="file:///F:/Users/parsh/Documents/Books/js-and-css/js/inject.js"></script></head>'%title, content)
+                    aCont = '<a href="toc01.html">Contents</a>'
+                    if jArr == 0:
+                        a3 = aCont + '<span> | </span><a href="' + arr[jArr + 1] + '">Next &gt;</a>'
+                    elif jArr >= leng - 1:
+                        a3 = '<a href="' + arr[jArr - 1] + '">&lt; Previous</a><span> | </span>' + aCont
+                    else:
+                        a3 = '<a href="' + arr[jArr - 1] + '">&lt; Previous</a><span> | </span>' + aCont + '<span> | </span><a href="' + arr[jArr + 1] + '">Next &gt;</a>'
+                    jArr = jArr + 1
+                    content = re.sub(r'<body>', r'<body><nav id="top-nav">%s</nav>'%a3, content)
                     f.write(content)
             else:
                 with open(doc, 'wb') as f:
