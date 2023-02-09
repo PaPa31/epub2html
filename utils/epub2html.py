ee746268e04fde0f151d508906a060595f4ca018
papa <visidoor@yandex.ru>
2023-02-09 13:43:25 +0500

    fix: XTML issue

--- utils/epub2html.py
+++ utils/epub2html.py
@@ -18,26 +18,27 @@ def convert(path):
     html_dir.mkdir(exist_ok=True)
     book = epub.read_epub(path)
     arr = []
-    iArr = 0
+    ext = ''
     for item in book.get_items():
         if item.get_type() == ebooklib.ITEM_DOCUMENT:
             arr.append(item.get_name())
-            iArr = iArr + 1
+            if ext == '':
+                ext = os.path.splitext(item.get_name())[1]
     leng = len(arr)
     jArr = 0
     a3 = ''
+    aCont = '<a href="toc01' + ext + '">Contents</a>'
     for item in book.get_items():
         doc = html_dir / item.get_name()
         os.makedirs(os.path.dirname(doc), exist_ok=True)
         content = item.get_content()
         if item.get_type() != ebooklib.ITEM_IMAGE:
             if item.get_type() == ebooklib.ITEM_DOCUMENT:
+                title = os.path.splitext(item.get_name())[0]
                 with open(doc, 'w') as f:
-                    title = os.path.splitext(item.get_name())[0]
                     content = content.decode()
                     content = re.sub(r'<a (.+?)\/>', r'<a \1></a>', content)
                     content = re.sub(r'<head/>', r'<head><title>%s</title><script type="text/javascript" src="/js-and-css/js/inject.js"></script><script type="text/javascript" src="file:///media/storage418Gb/Users/parsh/Documents/Books/js-and-css/js/inject.js"></script><script type="text/javascript" src="file:///F:/Users/parsh/Documents/Books/js-and-css/js/inject.js"></script></head>'%title, content)
-                    aCont = '<a href="toc01.html">Contents</a>'
                     if jArr == 0:
                         a3 = aCont + '<span> | </span><a href="' + arr[jArr + 1] + '">Next &gt;</a>'
                     elif jArr >= leng - 1:
@@ -47,8 +48,7 @@ def convert(path):
                     jArr = jArr + 1
                     content = re.sub(r'<body>', r'<body><header>%s</header>'%a3, content)
                     if title == 'toc01':
-                        print('toc01')
-                        content = content.replace('</head>', f'<link href="/js-and-css/css/collapsible-style.css" rel="stylesheet" type="text/css"><link href="file:///media/storage418Gb/Users/parsh/Documents/Books/js-and-css/css/collapsible-style.css" rel="stylesheet" type="text/css"><link href="file:///F:/Users/parsh/Documents/Books/js-and-css/css/collapsible-style.css" rel="stylesheet" type="text/css"></head>')
+                        content = content.replace('</head>', f'<link href="/js-and-css/css/collapsible-style.css" rel="stylesheet" type="text/css"></link><link href="file:///media/storage418Gb/Users/parsh/Documents/Books/js-and-css/css/collapsible-style.css" rel="stylesheet" type="text/css"></link><link href="file:///F:/Users/parsh/Documents/Books/js-and-css/css/collapsible-style.css" rel="stylesheet" type="text/css"></link></head>')
                         content = content.replace('</body>', f'<script type="text/javascript" src="/js-and-css/js/collapsible-script.js"></script><script type="text/javascript" src="file:///media/storage418Gb/Users/parsh/Documents/Books/js-and-css/js/collapsible-script.js"></script><script type="text/javascript" src="file:///F:/Users/parsh/Documents/Books/js-and-css/js/collapsible-script.js"></script></body>')
                     f.write(content)
             else:
@@ -57,4 +57,5 @@ def convert(path):
         else:
             with open(doc, 'wb') as f:
                 f.write(content)
-    print(f'"{html_dir}" created.')
+    print(f'"{html_dir}" folder created.')
+    print(f'{html_dir}/toc01{ext} - start page with Table of Content')
