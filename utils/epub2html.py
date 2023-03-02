266d871928ae7d81cbb2bf23a0f71abc4365fa4c
papa <visidoor@yandex.ru>
2023-03-02 16:09:13 +0500

    chore: detect toc file

--- utils/epub2html.py
+++ utils/epub2html.py
@@ -18,16 +18,24 @@ def convert(path):
     html_dir.mkdir(exist_ok=True)
     book = epub.read_epub(path)
     arr = []
-    ext = ''
+    #ext = ''
+    toc = ''
     for item in book.get_items():
         if item.get_type() == ebooklib.ITEM_DOCUMENT:
             arr.append(item.get_name())
-            if ext == '':
-                ext = os.path.splitext(item.get_name())[1]
+            #if ext == '':
+            #    ext = os.path.splitext(item.get_name())[1]
+            if toc == '' and item.get_name()[0:3] == 'toc':
+                toc = item.get_name()
+    if toc != '':
+        print('Find toc file!')
+    else:
+        print('Could not find toc file!')
+        return
     leng = len(arr)
     jArr = 0
     a3 = ''
-    aCont = '<a href="toc01' + ext + '">Contents</a>'
+    aCont = '<a href="' + toc + '">Contents</a>'
     for item in book.get_items():
         doc = html_dir / item.get_name()
         os.makedirs(os.path.dirname(doc), exist_ok=True)
@@ -47,7 +55,8 @@ def convert(path):
                         a3 = '<a href="' + arr[jArr - 1] + '">&lt; Previous</a><span> | </span>' + aCont + '<span> | </span><a href="' + arr[jArr + 1] + '">Next &gt;</a>'
                     jArr = jArr + 1
                     content = re.sub(r'<body>', r'<body><header>%s</header>'%a3, content)
-                    if title == 'toc01':
+                    #if title == 'toc01':
+                    if title[0:3] == 'toc':
                         content = content.replace('</head>', f'<link href="/js-and-css/css/collapsible-style.css" rel="stylesheet" type="text/css"></link><link href="file:///media/storage418Gb/Users/parsh/Documents/Books/js-and-css/css/collapsible-style.css" rel="stylesheet" type="text/css"></link><link href="file:///F:/Users/parsh/Documents/Books/js-and-css/css/collapsible-style.css" rel="stylesheet" type="text/css"></link></head>')
                         content = content.replace('</body>', f'<script type="text/javascript" src="/js-and-css/js/collapsible-script.js"></script><script type="text/javascript" src="file:///media/storage418Gb/Users/parsh/Documents/Books/js-and-css/js/collapsible-script.js"></script><script type="text/javascript" src="file:///F:/Users/parsh/Documents/Books/js-and-css/js/collapsible-script.js"></script></body>')
                     f.write(content)
@@ -58,4 +67,5 @@ def convert(path):
             with open(doc, 'wb') as f:
                 f.write(content)
     print(f'"{html_dir}" folder created.')
-    print(f'{html_dir}/toc01{ext} - start page with Table of Content')
+    print(f'{html_dir}/{toc} - start page with Table of Content')
+
